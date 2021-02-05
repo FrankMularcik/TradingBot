ecb79ddf2ade8eb6a9fa0ef3f4747cc14ebce2d7
Frank Mularcik <63128305+FrankMularcik@users.noreply.github.com>
2021-02-05 18:58:44 -0500

    Update back_trader.py

--- back_trader.py
+++ back_trader.py
@@ -4,19 +4,7 @@ import strategies
 import pandas as pd
 import bars_alpaca
 
-#data_path = 'AAPL_1min_sample.csv'
-#data_file = pd.read_csv(data_path, index_col='Date', parse_dates=True)
 
-    
-
-
-# data = backtrader.feeds.YahooFinanceCSVData(
-#     dataname=data_path,
-#     # Do not pass values before this date
-#     fromdate=datetime.datetime(2016, 1, 1),
-#     # Do not pass values after this date
-#     todate=datetime.datetime(2021, 12, 31),
-#     reverse=False)
 starting_values = 0
 ending_values = 0
 output = open('backtrade.txt', 'w+')
@@ -27,7 +15,7 @@ for i in range(len(bars_alpaca.frames)):
     cerebro.adddata(data)
 
     cerebro.addstrategy(strategies.MyStrat)
-    #cerebro.broker.set_fundstartval(0)
+    
     cerebro.broker.set_cash(100000)
     #print("Current Portfolio Value: %.2f" % cerebro.broker.getvalue())
     ticker_start = cerebro.broker.getvalue()
